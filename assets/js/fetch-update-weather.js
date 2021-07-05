f8e84ac7df68033a95266a91268ede97cd6e8cdc
Brandon Mellus <mellusbrandon@gmail.com>
2021-07-04 21:57:29 -0400

    Add five day forecast

--- assets/js/fetch-update-weather.js
+++ assets/js/fetch-update-weather.js
@@ -17,9 +17,22 @@ async function fetchAndUpdateWeather(city) {
             moment(),
             data2.current.weather[0]
         );
-        console.log(data2)
+        const fiveDayForecast = [];
+        for (let i = 0; i < 5; i++) {
+            let date = moment();
+            date = date.date(date.date() + i + 1);
+            fiveDayForecast.push(
+                new WeatherData(
+                    data2.daily[i].temp.day,
+                    data2.daily[i],
+                    date,
+                    data2.daily[i].weather[0]
+                )
+            );
+        }
 
         displayCurrentWeather(city, currentWeather);
+        displayFiveDayForecast(fiveDayForecast);
     } catch(err) {
         throw err;
     }
@@ -28,10 +41,30 @@ async function fetchAndUpdateWeather(city) {
 function displayCurrentWeather(city, currentWeather) {
     $("#weather-main-city-name").text(city);
     $("#weather-main-date").text(currentWeather.date);
-    $("#weather-main-icon").attr("src", `${WEATHER_ICON}/${currentWeather.icon}`);
+    $("#weather-main-icon").attr("src", `${getIconFile(currentWeather.icon)}`);
     $("#weather-main-icon").attr("alt", currentWeather.iconDescription);
     $("#weather-main-temp").text(currentWeather.temp);
     $("#weather-main-wind").text(currentWeather.wind);
     $("#weather-main-humidity").text(currentWeather.humidity);
     $("#weather-main-uvi").text(currentWeather.uvi);
+}
+
+function displayFiveDayForecast(fiveDayForecast) {
+    $("#five-day-forecast ul").html("");
+    fiveDayForecast.forEach((weather) => {
+        const el = $(
+            `<li class="forecast-item>
+                <div class="date">${weather.date}</div>
+                <image class="icon" src=${getIconFile(weather.icon)} alt=${weather.description} />
+                <div class="temp">Temp: ${weather.temp}&deg; F</div>
+                <div class="wind">Wind: ${weather.wind} MPH</div>
+                <div class="humidity">Humidity: ${weather.humidity}%</div>
+            </li>`
+        )
+        $("#five-day-forecast ul").append(el);
+    });
+}
+
+function getIconFile(icon) {
+    return `${WEATHER_ICON}/${icon}`
 }
