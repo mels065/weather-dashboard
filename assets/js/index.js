696bbadc93caa606a10a47fdf8f4f4ff56a33e66
Brandon Mellus <mellusbrandon@gmail.com>
2021-07-05 00:04:56 -0400

    Add script that runs when page is opened

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,9 @@
+const city = localStorage.getItem("city");
+const searchHistory = new Set(
+    JSON.parse(localStorage.getItem("searchHistory"))
+);
+
+fetchAndUpdateWeather(city);
 displaySearchHistory();
 
 $("#search-city-form").submit(searchCity);
